026982e5e12697c2c6f8ed997a9fbe7764ad565b
xuxiaohaoer <bzxuhao@163.com>
2020-09-16 18:02:58 +0800

    feature_product_3

--- data_product.py
+++ data_product.py
@@ -2,12 +2,17 @@ import pre
 import _json
 import json
 import numpy as np
-# x_black = pre.pre_pcap("data/eta_1/train/black/")
-# x_white = pre.pre_pcap("data/eta_1/train/white/")
-x_black = [[1, 2, 3], [2, 3, 4]]
+x_black = pre.pre_pcap("data/eta_1/train/black/")
+x_white = pre.pre_pcap("data/eta_1/train/white/")
 f = open("feature_list_b", 'w+')
 x_b = json.dumps(x_black)
 f.write(x_b)
 f.close()
+f = open("feature_list_w", 'w+')
+x_w = json.dumps(x_white)
+f.write(x_w)
+f.close()
+
+
 
 
--- data_read.py
+++ data_read.py
@@ -1,6 +1,11 @@
 import json
-
-with open('feature_lsit_b', 'r') as f:
-    tem = f.read()
-    x_black = json.loads(tem)
-    print(x_black)
+def data_read():
+    with open('feature_list_b', 'r') as f:
+        tem = f.read()
+        x_black = json.loads(tem)
+    f.close()
+    with open('feature_list_w', 'r') as f:
+        tem = f.read()
+        x_white = json.loads(tem)
+    f.close
+    return x_black, x_white

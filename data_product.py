035024ffb6cca021bd9d69790e0de57475b55a17
xuxiaohaoer <bzxuhao@163.com>
2020-09-16 17:55:52 +0800

    feature_product_1

--- data_product.py
+++ data_product.py
@@ -1,9 +1,13 @@
 import pre
+import _json
+import json
 import numpy as np
-x_black = pre.pre_pcap("data/eta_1/train/black/")
-x_white = pre.pre_pcap("data/eta_1/train/white/")
+# x_black = pre.pre_pcap("data/eta_1/train/black/")
+# x_white = pre.pre_pcap("data/eta_1/train/white/")
+x_black = [[1, 2, 3], [2, 3, 4]]
+f = open("feature_list_b", 'w+')
+x_b = json.dumps(x_black)
+f.write(x_b)
+f.close()
 
-f = open("feature_list", 'w+')
-f.write(x_black)
-f.write(x_white)
 

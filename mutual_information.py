32ae95d88f61bc9462f1b41e5596c3fcfa337897
xuxiaohaoer <bzxuhao@163.com>
2020-09-16 15:58:35 +0800

    mutual

--- mutual_information.py
+++ mutual_information.py
@@ -1,13 +1,10 @@
 import dataset
 import numpy as np
 from sklearn import metrics
-# x_train, y_train, x_test, y_label = dataset.pre_data()
-# x = x_train + x_test
-# y = y_train + y_label
-x = [[1,2,3], [2,3,4], [3,4,5]]
-y = [0,1,0]
-
+x_train, y_train, x_test, y_label = dataset.pre_data()
+x = x_train + x_test
+y = y_train + y_label
 tem = np.array(x)
-for i in range(x[0]):
-    mutual = metrics.mutual_info_score(x[:i], y)
-    print(i, tem)
+for i in range(len(tem[0])):
+    mutual = metrics.mutual_info_score(x[:, i], y)
+    print(i, mutual)
